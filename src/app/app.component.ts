d35e1ecc752bcc52e14233a69c651372b483aebf
Buljubasic <KI0V@icbc.net>
2020-06-10 15:00:27 -0700

    add custom validators, both sync and async.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -22,6 +23,8 @@ export class AppComponent implements OnInit {
   // for template driven approach.  ReactiveFormsModule contains all tools we need to build
   // our form programatically and connect it to html code.
   signupForm: FormGroup;
+  // We create custom validators to prevent adding these two names as user names
+  forbiddenUserNames = ['Chris', 'Anna'];
 
   ngOnInit(): void {
     // since there is quite lots of code to create programmatically form and since we need
@@ -39,8 +42,8 @@ export class AppComponent implements OnInit {
       // at that time.  Note we can also have FormGroup inside another FormGroup to group
       // controls.
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'username': new FormControl(null, [Validators.required, this.forbiddenNamesValidator.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmailsAsyncValidator.bind(this))
       }),
       'gender': new FormControl('male'), 
       // here we add FormArray to add group of controls
@@ -55,12 +58,69 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     // Here we add control without any value since user is supposed to provide that
     // but we add validator since hobby should be requred to provide if user clicks
-    // on the button.
+    // on the button.  IMPORTANT: note how I bind 'this' to by forbiddenNamesValidator.
+    // This is using good old javascript sintax to do binding and it is needed because
+    // the refernce to 'this' inside forbiddenNamesValidator() is not reference to this
+    // class but rather Angular runtime environment becaseuse Angular calls this method
+    // when it does validation.
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
 
+  // Here we create a getter method to return controls from 'hobbies' form array which
+  // we access in template.  Note how we first have to cast it to FormArray in order to
+  // get access to its controls.
   get controls() {
     return (this.signupForm.get('hobbies') as FormArray).controls;
+  }
+
+  // A custom validator is very easy to create, it is just a function
+  // which gets executed by the Angular automatically whenever it checks
+  // validity of that control which is whenever you change its value.
+  // For a validator to work correctly, it needs to receive control which 
+  // it should check and it needs to return something for Angular to be able
+  // to handle the return value correctly.
+  // This something should be a JS object having any key of type string (note
+  // this is just ts syntax for saying we want to have key / value pair where
+  // key is of type string) and return type boolean.
+  forbiddenNamesValidator(control: FormControl): {[s: string]: boolean} {
+    // this should return something like {nameIsForbidden: true}
+    // check if value of control is part of our forbiddenUserName
+    // IMPORTANT: Who is calling this method?  Angular at times it check validity.
+    // THEREFORE: 'this' will not be this class so we cannot use 'this' here.  To
+    // fix this, we bind 'this' when we add this validator to Formcontrol above.
+    // NOTE however that this checking if control value is part of this array
+    // will return -1 if that is not the case, but -1 in boolean is True, so we 
+    // need to ensure this is not equal to -1.  If not equal to -1, that means
+    // we found it and that means it is invalid.
+    if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    // IMPORTANT: if validation is successfull, you should pass null or nothing.
+    // You should not pass something like {nameIsForbidden: false}.
+    return null;
+  }
+
+  // Let's create async validator now in case validation takes some time, say it
+  // needs to reach server.  but in this case, we dont return key/value pair of
+  // boolean but a Promise of anything or Observable of anything.  These are
+  // constructs that handle async data.  NOTE that if you were using 'this' here,
+  // we would have to bind it to 'this' instance of this class (which I did above
+  // when adding this async validator to FormControl), but it is not needed to
+  // do that in this case since I am not using 'this' below.
+  forbiddenEmailsAsyncValidator(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          // so if email is invalid we return this object like in sync validator but
+          // in async we dont return, we resolve instaead.
+          resolve({'emailIsForbidden': true});
+        } else {
+          // valid email address
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
